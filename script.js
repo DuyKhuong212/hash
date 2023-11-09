3bf8c233cdbcf0438e9883244f792a217e41c3fc
DuyKhuong212 <nguyenduykhuong0201t@gmail.com>
2023-11-09 09:11:20 +0700

    fix

--- script.js
+++ script.js
@@ -18,25 +18,58 @@ function decryptText() {
 }
 
 // Hàm để mở modal
+const modal = document.getElementById('modal');
+const modalText = document.getElementById('modalText');
 function openModal(text) {
-  const modal = document.getElementById('modal');
-  const modalText = document.getElementById('modalText');
   modal.style.display = 'block';
-  if (text.length > 20) {
-    modalText.innerHTML = text.slice(0, 20) + '<br>' + text.slice(20);
-    return;
-  }
-  
   modalText.textContent = text;
 }
 
+modalText.addEventListener('click', function () {
+  // Tạo một vùng chọn (range) cho nội dung modalText
+  const range = document.createRange();
+  range.selectNodeContents(modalText);
+
+  // Lựa chọn nội dung trong vùng chọn
+  const selection = window.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+
+  // Sao chép nội dung đã chọn
+  document.execCommand('copy');
+
+  // Bỏ chọn sau khi sao chép
+  selection.removeAllRanges();
+
+  // Thông báo cho người dùng nếu nội dung đã được sao chép
+  alert('Nội dung đã được sao chép!');
+  return;
+});
+
 // Hàm để đóng modal
 function closeModal() {
-  const modal = document.getElementById('modal');
-  const modalText = document.getElementById('modalText');
   modal.style.display = 'none';
   modalText.textContent = '';
 }
+
+// Hàm để cập nhật kích thước modalText dựa trên kích thước hiện tại của màn hình
+function updateModalTextSize() {
+  const modalText = document.getElementById('modalText');
+  const screenWidth = window.innerWidth;
+
+  // Điều chỉnh kích thước tùy thuộc vào kích thước màn hình
+  if (screenWidth < 768) {
+    modalText.style.fontSize = '14px'; // Điều chỉnh kích thước phù hợp
+  } else {
+    modalText.style.fontSize = '16px'; // Điều chỉnh kích thước phù hợp cho màn hình lớn hơn
+  }
+}
+
+// Thêm sự kiện resize để theo dõi thay đổi kích thước màn hình
+window.addEventListener('resize', updateModalTextSize);
+
+// Gọi hàm để cài đặt kích thước ban đầu
+updateModalTextSize();
 
 // Gắn sự kiện click cho nút mã hóa, giải mã, và đóng modal
 document.getElementById('encryptButton').addEventListener('click', encryptText);
